92d758000da73e0e28728d9719bbfc7069444581
Şükrü Taha Bıyık <57368706+ultimate-dev@users.noreply.github.com>
2022-01-07 07:39:48 +0300

    Comments

--- airpollution-arduino/arduino/arduino.ino
+++ airpollution-arduino/arduino/arduino.ino
@@ -6,7 +6,6 @@
 #include <MQ135.h>
 #include <Adafruit_BMP085.h>
 #include <Adafruit_PCD8544.h>
-
 /**
  * Pins
  */
@@ -16,99 +15,89 @@
 #define PIN_MQ A1
 #define PIN_FAN 2
 #define PIN_PIEZO 3
-
 /**
  * Constants
  */
- const uint16_t DELAY = 500;
-
+const uint16_t DELAY = 500;
 /**
  * Initializing
  */
-SoftwareSerial espSerial(PIN_ESP_RX, PIN_ESP_TX);// (RX, TX)
-DHT dht(PIN_DHT, DHT11);// (Pin, DHT_Type)
-MQ135 mq(PIN_MQ);// (Pin)
-Adafruit_BMP085 bmp; // (SCL, SDA)->(A4, A5)
-Adafruit_PCD8544 lcd = Adafruit_PCD8544(8, 9, 10, 12, 11);// (SCLK, DIN, D/C, CS, RST)
-
- /**
+SoftwareSerial espSerial(PIN_ESP_RX, PIN_ESP_TX);          // (RX, TX)
+DHT dht(PIN_DHT, DHT11);                                   // (Pin, DHT_Type)
+MQ135 mq(PIN_MQ);                                          // (Pin)
+Adafruit_BMP085 bmp;                                       // (SCL, SDA)->(A4, A5)
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(8, 9, 10, 12, 11); // (SCLK, DIN, D/C, CS, RST)
+/**
  * Varibles
  */
- float lng, lat, altitude;
- float ppm,rzero,resistance, co_ppm, co2_ppm, alkol_ppm, aseton_ppm;
- float temperature, humidity, heat_index;
- float pressure,fan_temp;
+float lng, lat, altitude;
+float ppm, rzero, resistance, co_ppm, co2_ppm, alkol_ppm, aseton_ppm;
+float temperature, humidity, heat_index;
+float pressure, fan_temp;
 
-
-void setup() {
-
+void setup()
+{
+  // Ouputs initializing
   pinMode(PIN_FAN, OUTPUT);
   pinMode(PIN_PIEZO, OUTPUT);
-  
+  // Seial comminications
   Serial.begin(9600);
   espSerial.begin(115200);
-  
+  // Installing Modul and Sensors
   dht.begin();
   bmp.begin();
   lcd.begin();
   lcd.setContrast(100);
-
 }
 
-void loop() {
-
-
- // DHT11
+void loop()
+{
+  // DHT11
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
   heat_index = dht.computeHeatIndex(temperature, humidity, false);
- 
   // MQ135
   rzero = mq.getCorrectedRZero(temperature, humidity);
   resistance = mq.getCorrectedResistance(temperature, humidity);
   ppm = mq.getCorrectedPPM(temperature, humidity);
-  co_ppm=calcPpm(rzero, resistance, -0.250, 0.687);
-  co2_ppm=calcPpm(rzero, resistance, -0.351, 0.721);
-  alkol_ppm=calcPpm(rzero, resistance,-0.316, 0.681);
-  aseton_ppm=calcPpm(rzero, resistance, -0.298, 0.465);
-
+  co_ppm = calcPpm(rzero, resistance, -0.250, 0.687);
+  co2_ppm = calcPpm(rzero, resistance, -0.351, 0.721);
+  alkol_ppm = calcPpm(rzero, resistance, -0.316, 0.681);
+  aseton_ppm = calcPpm(rzero, resistance, -0.298, 0.465);
   // BMP180
   pressure = bmp.readPressure();
   fan_temp = bmp.readTemperature();
-  
- // 3310LCD
+  // 3310LCD
   lcd.clearDisplay();
   lcd.setTextSize(1);
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.println("Hello, world!");
   lcd.display();
-
   // Fan
-  if(fan_temp>35.00){
+  if (fan_temp > 35.00)
     digitalWrite(PIN_FAN, HIGH);
-  }else{
+  else
     digitalWrite(PIN_FAN, LOW);
-  }
-
-  
- 
-
-  espSerial.print("\"ppm\":"+String(ppm,2));
-  espSerial.print(", \"co_ppm\":"+String(co_ppm,2));
-  espSerial.print(", \"co2_ppm\":"+String(co2_ppm,2));
-  espSerial.print(", \"alkol_ppm\":"+String(alkol_ppm,2));
-  espSerial.print(", \"aseton_ppm\":"+String(aseton_ppm,2));
-  espSerial.print(", \"temperature\":"+String(temperature,2));
-  espSerial.print(", \"humidity\":"+String(humidity,2));
-  espSerial.print(", \"heat_index\":"+String(heat_index,2));
-  espSerial.print(", \"pressure\":"+String(pressure,4));
+  // Sends sensor and module data via Serial Communication
+  espSerial.print("\"ppm\":" + String(ppm, 2));
+  espSerial.print(", \"co_ppm\":" + String(co_ppm, 2));
+  espSerial.print(", \"co2_ppm\":" + String(co2_ppm, 2));
+  espSerial.print(", \"alkol_ppm\":" + String(alkol_ppm, 2));
+  espSerial.print(", \"aseton_ppm\":" + String(aseton_ppm, 2));
+  espSerial.print(", \"temperature\":" + String(temperature, 2));
+  espSerial.print(", \"humidity\":" + String(humidity, 2));
+  espSerial.print(", \"heat_index\":" + String(heat_index, 2));
+  espSerial.print(", \"pressure\":" + String(pressure, 4));
   espSerial.println();
- 
-
+  // Wait until the delay
   delay(DELAY);
 }
 
-
-double calcPpm(float rzero, float resistance,double m, double b){
-  return pow(10, (log10(resistance/rzero)-b)/m);
+/**
+ * Functions
+ */
+// Ppm values calculation
+double calcPpm(float rzero, float resistance, double m, double b)
+{
+  return pow(10, (log10(resistance / rzero) - b) / m);
 }
--- airpollution-arduino/esp8266/esp8266.ino
+++ airpollution-arduino/esp8266/esp8266.ino
@@ -5,98 +5,103 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <TinyGPS.h>
-
 /**
  * Pins
  */
 #define PIN_GPS_RX 4
 #define PIN_GPS_TX 5
-
 /**
  * Configrations
  */
- const String ID = "ELZ_1";
- const String WIFI_SSID = "AirPollution_WIFI";
- const String WIFI_PASS = "taha55taha";
- const String SERVER_HOST = "http://192.168.43.185:2323";
-
- /**
+const String ID = "ELZ_1";
+const String WIFI_SSID = "AirPollution_WIFI";
+const String WIFI_PASS = "taha55taha";
+const String SERVER_HOST = "http://192.168.43.185:2323";
+/**
  * Constants
  */
- const uint16_t DELAY = 500;
-
+const uint16_t DELAY = 500;
 /**
  * Initializing
  */
- TinyGPS gps;
- SoftwareSerial gpsSerial(PIN_GPS_RX, PIN_GPS_TX);// (RX, TX)
- 
+TinyGPS gps;
+SoftwareSerial gpsSerial(PIN_GPS_RX, PIN_GPS_TX); // (RX, TX)
 /**
- * Varibles
+ * Variables
  */
- float lng, lat, altitude;
- float ppm, co_ppm, co2_ppm, alkol_ppm, aseton_ppm;
- float temperature, humidity, heat_index;
- float pressure;
- String json;
- 
+float lng, lat, altitude;
+float ppm, co_ppm, co2_ppm, alkol_ppm, aseton_ppm;
+float temperature, humidity, heat_index;
+float pressure;
+String json;
 
-void setup() {
- Serial.begin(115200);
- gpsSerial.begin(9600);
+void setup()
+{
+  // Serial comminications
+  Serial.begin(115200);
+  gpsSerial.begin(9600);
+  // Installing modules
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.println();
-  while (WiFi.status() != WL_CONNECTED){
+  // Wifi connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 }
 
-void loop() {
-  
-   encodeGps();
-   
-   String json = encodeSerial();
-   Serial.println(json);
-
-
-   gps.f_get_position(&lat, &lng);
-   altitude = gps.f_altitude();
-
-  
-   if (WiFi.status() == WL_CONNECTED) {
-    
-      WiFiClient client;
-      HTTPClient http;
-    
-      http.begin(client, SERVER_HOST + "/api");
-      http.addHeader("Content-Type", "application/json");
-
-        
-      int httpCode = http.POST(json);
-      if(httpCode == HTTP_CODE_OK){
-        Serial.println(http.getString());
-      }
-      http.end();
-    }
-
-
+void loop()
+{
+  // Encoding
+  encodeGps();
+  // Serial comminication
+  String json = encodeSerial();
+  Serial.println(json);
+  // Gps latitude ,longitude and altitude values
+  gps.f_get_position(&lat, &lng);
+  altitude = gps.f_altitude();
+  // Wifi Connection status
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    // Wifi client and http classes
+    WiFiClient client;
+    HTTPClient http;
+    // Http request
+    http.begin(client, SERVER_HOST + "/api");
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(json);
+    // Http response
+    if (httpCode == HTTP_CODE_OK)
+      Serial.println(http.getString());
+    // Http end
+    http.end();
+  }
 }
 
-void encodeGps(){
+/**
+ * Functions
+ */
+
+// Encoding gps data
+void encodeGps()
+{
   unsigned long start = millis();
-  do{
-    while(gpsSerial.available())
+  do
+  {
+    while (gpsSerial.available())
       gps.encode(gpsSerial.read());
-  }while(millis()-start<DELAY);
+  } while (millis() - start < DELAY);
 }
 
-String encodeSerial(){
+// Encoding Serial comminication read data
+String encodeSerial()
+{
   String str;
   unsigned long start = millis();
 
-    if(Serial.available())
-      str+= Serial.readStringUntil('\r');
-  
-  return "{ \"id\":\""+ID+"\", \"lat\":"+String(lat,6)+", \"lng\":"+String(lng,6)+", \"altitude\":"+String(altitude,4)+", "+str+" }";
+  if (Serial.available())
+    str += Serial.readStringUntil('\r');
+
+  return "{ \"id\":\"" + ID + "\", \"lat\":" + String(lat, 6) + ", \"lng\":" + String(lng, 6) + ", \"altitude\":" + String(altitude, 4) + ", " + str + " }";
 }
